acfd12ac2e80d05e070273f63bd4d60fe6785970
Spencer Dixon <spencercdixon@gmail.com>
2016-07-29 10:39:16 -0400

    Adds example JSDoc notation to see how well the auto generated site with doclets looks

--- src/predicates.js
+++ src/predicates.js
@@ -3,23 +3,34 @@ import {
   has,
 } from 'lodash';
 
+/**
+ * Predicate to determine if value is a number
+ * @param {*} value - value to be tested
+ * @returns {Boolean} - whether or not value is a number
+*/
 export function isNumber(value) {
   return typeof value === 'number';
 }
 
+/** Predicate to determine if value is a string */
 export function isString(value) {
   return typeof value === 'string';
 }
 
+/** Predicate to determine if value is an array */
 export function isArray(value) {
   return Array.isArray(value);
 }
 
+/** Predicate to determine if value is an array or string (i.e. indexable) */
 export function isIndexed(value) {
   return isArray(value) ||  isString(value);
 }
 
+/** Predicate to determine if value is even */
 export function isEven(val) { return val % 2 === 0 }
+
+/** Predicate to determine if value is odd */
 export function isOdd(val)  { return !isEven(val) }
 
 export function hasKeys(...keys) {
